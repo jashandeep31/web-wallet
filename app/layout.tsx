f15e72d1a91a1e8bfa7e5cd7c120667d9641bcf6
jashandeep <jashandeep16592gmail.com>
2024-08-17 07:23:18 +0530

    fix: added dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <Toaster richColors />
         <div className="min-h-screen flex flex-col">
